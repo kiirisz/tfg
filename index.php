557c5db9470ea5b6332d074c85be9dc79d1802ef
AlbertoLagar <125266213+AlbertoLagar@users.noreply.github.com>
2023-05-10 19:13:19 +0200

    Big changes
    Created the phps, changed sidebar, changed a bit of styling

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include("template/cabecera.php"); ?>
+
+        <!-- dynamically generated thing goes here; for the time let's just write some text so as to signify you aren't following anyone -->
+        <!-- TODO: this should hide once you're following someone -->
+        <main class="h-full bg-slate-200 w-full flex flex-col items-center">
+
+            <div class="text-slate-800 flex flex-col items-center text-center my-auto text-">
+                <p class="font-black hover:rotate-180 transition-all text-9xl w-fit">?</p>
+
+                <p class="text-2xl font-bold">
+                    You aren't following anyone yet!<br>
+                    Go to the <a href="#">Explore</a> page and follow a few accounts!    
+                </p>
+
+                <div class="text-lg" id="linktest">
+                    We suggest <a href="profile.php">@kiiri</a>, <a href="#">@ciudadanoejemplar</a> and <a href="#">@ili</a>, but we're a bit biased
+                </div>
+            </div>
+        </main>
+
+<?php include("template/pie.php"); ?>
