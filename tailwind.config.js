1839c9e2f9e422bc47b0e6b0d809c4bbd0b51df5
Ramiro Ángel <theramiroangel@gmail.com>
2023-06-01 20:03:48 +0200

    the long-awaited register page 	modified:   front-end/dist/output.css 	new file:   front-end/pages/register.php 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   content: [
     "front-end/html/*.{html,js}",
+    "front-end/js/*.{html,js}",
     "front-end/pages/*.php",
     "front-end/template/*.php",
-    "front-end/*.php"
+    "*.php"
   ],
     theme: {
     extend: {},
