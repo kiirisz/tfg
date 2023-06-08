89299fdf62561e43672f87b629b7b6da5ba3ef09
kiirisz <theramiroangel@gmail.com>
2023-06-08 20:22:26 +0200

    footer is now back to working, hooray

--- front-end/pages/addPost.php
+++ front-end/pages/addPost.php
@@ -3,8 +3,7 @@ include("../template/header.php");
 include("../../back-end/actions/add.php");
 ?>
 
-
-<main class="h-screen  w-full flex flex-col items-center bg-gradient-to-br from-pink-500 to-blue-500">
+<main class="h-screen w-full flex flex-col items-center bg-gradient-to-br from-red-500 to-yellow-500">
 
     <div class="text-slate-800 bg-slate-200 flex flex-col items-center text-center my-auto px-5 rounded-md p-5">
         <!-- i am ready and willing to delete php from existence if this doesn't work -->
@@ -21,7 +20,6 @@ include("../../back-end/actions/add.php");
                 class="bg-pink-500 hover:bg-pink-900 transition ease-in-out text-white p-2 font-bold rounded-md">
         </form>
     </div>
-</main>
 
 <?php
 include("../template/footer.php");
