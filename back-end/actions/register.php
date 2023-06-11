2628cee2b25b3583b3aa574c9196f8e2a6ea138a
kiirisz <theramiroangel@gmail.com>
2023-06-11 22:33:07 +0200

    register now saves images to the /profile folder

--- back-end/actions/register.php
+++ back-end/actions/register.php
@@ -1,11 +1,13 @@
 <?php
+// the register page !!!
+// here's hoping it works or something along those lines lmao
 
+// calling the database handler
 include("../../back-end/db/db.php");
 
 $date = date('Y-m-d', time());
 
 // añadir aqui los campos para completar la tabla users de instagram
-
 if (isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['username'])) {
     $query = "INSERT INTO users (email, password, userName, birthDate, creationDate, phoneNumber, profilePic) 
     VALUES (:email, :password, :userName, :birthDate, :creationDate, :phoneNumber, :profilePic)";
@@ -26,6 +28,45 @@ if (isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['username'])
     if ($stmt->execute()) {
         $message = 'Successfully created new user';
         header("Location: ../../front-end/pages/login.php");
+
+        $targetDir = '../db/uploads/profile/'; // upload directory
+        $targetFile = $targetDir . basename($_FILES['imginput']['name']); // the directory+the file
+        $uploadOk = 1;
+        $imginputType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION)); // this stores the extension
+
+        // auto-generate a name for the image based on the user name
+        $targetFile = $targetDir . $$_POST['username'] . '.' . $imginputType; // Generate a unique filename using the timestamp
+
+        // Check if the uploaded file is an image
+        $check = getimagesize($_FILES['imginput']['tmp_name']);
+        if ($check === false) {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+
+        // Check file size (needed, if we want to account for people being silly)
+        if ($_FILES['imginput']['size'] > 10000000) { // 10mb
+            echo "File is too large.";
+            $uploadOk = 0;
+        }
+
+        // Allow only specific file formats
+        $allowedFormats = array('jpg', 'jpeg', 'png');
+        if (!in_array($imginputType, $allowedFormats)) { // checking if the format is allowed
+            echo "Only JPG, JPEG, and PNG files are allowed.";
+            $uploadOk = 0;
+        }
+
+
+
+        // If all checks pass, move the uploaded file to the target directory
+        if ($uploadOk == 1) {
+            if (move_uploaded_file($_FILES['imginput']['tmp_name'], $targetFile)) {
+                echo "Image uploaded successfully.";
+            } else {
+                echo "Error uploading the image.";
+            }
+        }
 
     } else {
         $message = 'Sorry, there\'s been an issue creating your account';
